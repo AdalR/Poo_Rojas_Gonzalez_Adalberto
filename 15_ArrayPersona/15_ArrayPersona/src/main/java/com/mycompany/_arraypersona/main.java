9b74dca79471eae3010242e04baabbaab42b5220
AdalR <a.rojas.g2@edu.utc.mx>
2021-12-05 14:25:54 -0600

    trabajo en clase

---
+++ 15_ArrayPersona/15_ArrayPersona/src/main/java/com/mycompany/_arraypersona/main.java
@@ -0,0 +1,10 @@
+package com.mycompany._arraypersona;
+
+public class main {
+    
+    
+    public static void main(String[] args){
+        ManipularPersona menu = new ManipularPersona();
+        menu.Principal();
+    }
+}
